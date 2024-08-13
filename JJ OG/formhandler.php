f3edda59d631dddf3035554ae64f60a8cddd4b53
ranrej <rania.rejdal@ontariotechu.net>
2024-08-12 22:50:18 -0400

    First push

---
+++ JJ OG/formhandler.php
@@ -0,0 +1,17 @@
+<?php
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $email_from = 'abdulmohsin113@gmail.com';
+        $email_subject = 'New Form Submission';
+        $email_body = "User Name: $name.\n".
+                        "User Email: $email.\n".
+                        "User Message: $message.\n";
+        $to=  'abdulmohsin113@gmail.com';
+        $headers= "From: $email_from \r\n"; 
+        $headers . "Reply To: $email \r\n";
+        
+        mail($to,$email_subject,$email_body,$headers);
+        header("Location: contact.html");
+
+?>
